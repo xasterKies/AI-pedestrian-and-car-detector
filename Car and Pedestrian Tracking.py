5744468d5f567390a13d1bd30fc1035ae0076fb8
SamuelEdward <samueledward40@gmail.com>
2020-08-23 09:36:29 -0700

    c

--- Car and Pedestrian Tracking.py
+++ Car and Pedestrian Tracking.py
@@ -25,94 +25,94 @@ while True:
     cars = car_tracker.detectMultiScale(grayscaled_frame)
 
     print(cars)
-    #Draw rectangles around the cars
-    for(x, y, w, h) in cars:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 225), 2)
-    # display video
-    cv2.imshow('Car Detector',frame)
-    #Dont autoclose (Wait here in the code and listen for a key press)
-    cv2.waitKey(1)
-    print("Code Complete")
+    # #Draw rectangles around the cars
+    # for(x, y, w, h) in cars:
+    #     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 225), 2)
+    # # display video
+    # cv2.imshow('Car Detector',frame)
+    # #Dont autoclose (Wait here in the code and listen for a key press)
+    # cv2.waitKey(1)
+    # print("Code Complete")
 
 
     
 
-""""
-#create an opencv image
-img = cv2.imread(img_file)
+# """"
+# #create an opencv image
+# img = cv2.imread(img_file)
 
 
-# convert to grayscale (need for here cascade)
-black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# # convert to grayscale (need for here cascade)
+# black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-#create car classifier
-car_tracker = cv2.CascadeClassifier(classifier_file)
+# #create car classifier
+# car_tracker = cv2.CascadeClassifier(classifier_file)
 
-#detect car
-cars = car_tracker.detectMultiScale(black_n_white)
+# #detect car
+# cars = car_tracker.detectMultiScale(black_n_white)
 
-#Draw rectangles around the cars
-for(x, y, w, h) in cars:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 225), 2)
-
-
-#display the image with the faces spotted
-cv2.imshow('Car Detector', img)
+# #Draw rectangles around the cars
+# for(x, y, w, h) in cars:
+#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 225), 2)
 
 
-#Dont autoclose (Wait here in the code and listen for a key press)
-cv2.waitKey()
+# #display the image with the faces spotted
+# cv2.imshow('Car Detector', img)
 
 
-print("Code Complete")
+# #Dont autoclose (Wait here in the code and listen for a key press)
+# cv2.waitKey()
 
 
-"""
-
-#image detection
-""""
-import cv2
-
-#Our Image
-img_file = 'car_image.png'
-video = cv2.VideoCapture('Pedestrian Compilation.mp4')
-
-#Our pre trained car classifier
-classifier_file = 'car_dector.xml'
-
-#Runs forever
-while True:
-
-    #Read the current frame
-    read_successful, frame = video.read()
-
-#create an opencv image
-img = cv2.imread(img_file)
+# print("Code Complete")
 
 
-# convert to grayscale (need for here cascade)
-black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# """
+
+# #image detection
+# """"
+# import cv2
+
+# #Our Image
+# img_file = 'car_image.png'
+# video = cv2.VideoCapture('Pedestrian Compilation.mp4')
+
+# #Our pre trained car classifier
+# classifier_file = 'car_dector.xml'
+
+# #Runs forever
+# while True:
+
+#     #Read the current frame
+#     read_successful, frame = video.read()
+
+# #create an opencv image
+# img = cv2.imread(img_file)
 
 
-#create car classifier
-car_tracker = cv2.CascadeClassifier(classifier_file)
-
-#detect car
-cars = car_tracker.detectMultiScale(black_n_white)
-
-#Draw rectangles around the cars
-for(x, y, w, h) in cars:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 225), 2)
+# # convert to grayscale (need for here cascade)
+# black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-#display the image with the faces spotted
-cv2.imshow('Car Detector', img)
+# #create car classifier
+# car_tracker = cv2.CascadeClassifier(classifier_file)
+
+# #detect car
+# cars = car_tracker.detectMultiScale(black_n_white)
+
+# #Draw rectangles around the cars
+# for(x, y, w, h) in cars:
+#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 225), 2)
 
 
-#Dont autoclose (Wait here in the code and listen for a key press)
-cv2.waitKey()
+# #display the image with the faces spotted
+# cv2.imshow('Car Detector', img)
 
 
-print("Code Complete")
-"""
+# #Dont autoclose (Wait here in the code and listen for a key press)
+# cv2.waitKey()
+
+
+# print("Code Complete")
+# """
