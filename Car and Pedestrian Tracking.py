4d4fda624a4092a1b4c108558e867cce60ccfc29
SamuelEdward <samueledward40@gmail.com>
2020-08-23 09:39:34 -0700

    c

--- Car and Pedestrian Tracking.py
+++ Car and Pedestrian Tracking.py
@@ -31,7 +31,7 @@ while True:
     # # display video
     cv2.imshow('Car Detector',grayscaled_frame)
     #Dont autoclose (Wait here in the code and listen for a key press)
-    cv2.waitKey(1)
+    cv2.waitKey()
     print("Code Complete")
 
 
