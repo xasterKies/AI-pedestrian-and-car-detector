659e0266cf31ac27ab61da8400c6c01d062e0d8e
SamuelEdward <samueledward40@gmail.com>
2020-08-23 00:28:18 -0700

    p

--- Car and Pedestrian Tracking.py
+++ Car and Pedestrian Tracking.py
@@ -11,16 +11,17 @@ clasifier_file = 'car_dector.xml'
 img = cv2.imread(img_file)
 
 
-#create car classifier
-car_tracker = cv2.CascadeClasssifier(classifier_file)
-
-
 # convert to grayscale (need for here cascade)
 black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
+#create car classifier
+car_tracker = cv2.CascadeClasssifier(classifier_file)
+
+
+
 #display the image with the faces spotted
-cv2.imshow('Car Detector', img)
+cv2.imshow('Car Detector', black_n_white)
 
 
 #Dont autoclose (Wait here in the code and listen for a key press)
